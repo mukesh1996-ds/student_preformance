e875b947780c0470012e4828e26011015a3e2384
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-04-09 03:42:19 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from src.student_performace.logger import logging
 from src.student_performace.exception import CustomeException
+from src.student_performace.components.data_ingestion import DataIngestion
 import sys 
 
 if __name__=="__main__":
     logging.info("The execution is started")
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custome Exception")
         raise CustomeException(e,sys)
