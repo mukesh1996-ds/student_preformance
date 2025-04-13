cd2268815810b3dfd058cf07fe80f4f95fe91290
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-04-13 10:32:43 +0530

    model trained

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from src.student_performace.logger import logging
 from src.student_performace.exception import CustomeException
 from src.student_performace.components.data_ingestion import DataIngestion
 from src.student_performace.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.student_performace.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
 import sys 
 
 if __name__=="__main__":
@@ -10,9 +12,14 @@ if __name__=="__main__":
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        ## model training 
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info("Custome Exception")
